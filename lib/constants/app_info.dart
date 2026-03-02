d4f2a67b5e78d4206416f30d1727f764481b8eee
adnanullah <daaztanbd71@gmail.com>
2026-03-02 16:56:47 +0600

    Update database_name to meditest_new

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -6,6 +6,6 @@ final meditest = "meditest";
 final meditest_stage = "meditest_stage";
 
 final meditest_new = "meditest_new";
-final meditest_new_stage = "meditest_new_stage";
+final meditest_new_stage = "meditest_new";
 final database_name = meditest_new_stage;
 late final superUser;
